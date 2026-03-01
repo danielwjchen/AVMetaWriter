dd17820444f88fa97b2daa63ce97f51b8ce02e57
Daniel Chen <hello@danielwjchen.com>
2026-03-01 14:55:56 -0600

    Initial commit.

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "AVMetaWritter",
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0")
+
+  ],
+  targets: [
+    .executableTarget(
+      name: "AVMetaWritter",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser")
+      ],
+    )
+  ]
+)
